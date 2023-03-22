31e242e8c683b22dad0c4470e22143aca48cbee9
MichaelGreason <mgreason18@gmail.com>
2023-03-22 17:35:30 -0400

    answers appear and question cycle on correct answer click, score also adds, wokring on shuffling the answers

--- react-trivia-michaelgreason/src/gamephase1.js
+++ react-trivia-michaelgreason/src/gamephase1.js
@@ -1,20 +1,35 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
-import Play from './select-question.js'
 
 
 function GamePhase1({categoryId}){
-    const [question, setQuestion] = useState(null)
-    const [selectQuestion, setSelectQuestion] = useState(null)
-
+    const [questions, setQuestions] = useState(null)
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [allAnswers, setAllAnswers] = useState(null)
+    let score = useRef(0)
+    let question 
+    
     useEffect(() => {
         console.log('useEffect runs')
         const URL = `https://opentdb.com/api.php?amount=10&category=${categoryId}`
         axios.get(URL).then((response) => { 
-                console.log(response.data)
-             setQuestion(response.data)}) }, [])
-        
-    
+            console.log(response.data)               
+            setQuestions(response.data)}) 
+            score.current = 0
+            // setAllAnswers = [question.incorrect_answers, ...question.incorrect_answers]
+            // console.log(setAllAnswers)
+            
+            
+            }, [])
+            
+            function handleCorrect() {
+                score.current += 1
+                setCurrentQuestion(setCurrentQuestion => setCurrentQuestion+1)
+            }
+
+            if (questions) question = questions.results[currentQuestion]
+            console.log(question)
+            
     return (
         
         <>
@@ -22,10 +37,15 @@ function GamePhase1({categoryId}){
         <h3>
             Question: 
         </h3>
+        <h2>{score.current}</h2>
         <div>
-            {question && (question.results.map((result, ind) => <li key={ind}><button onClick={() => setSelectQuestion(result.question)}>{result.question}</button></li>))}
+            {questions && <div>{question.question}</div>}
         </div>
-        {selectQuestion && <Play selectQuestion={selectQuestion} question={question}/>}
+        <div>
+            {questions && <li><button onClick={handleCorrect}>{question.correct_answer}</button></li>}
+            {questions && question.incorrect_answers.map((wrong) => <li><button>{wrong}</button></li>)}
+        </div>
+        
         </>
         
     )
