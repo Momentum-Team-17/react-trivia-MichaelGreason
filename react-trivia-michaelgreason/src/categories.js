fe4b2bf6a43b2974c2efcf44a74921df368ed1d9
MichaelGreason <mgreason18@gmail.com>
2023-03-24 15:11:44 -0400

    moving on to styling

--- react-trivia-michaelgreason/src/categories.js
+++ react-trivia-michaelgreason/src/categories.js
@@ -13,8 +13,13 @@ function Categories({setCategoryId}) {
 
     return (
         <>
-        <h1>PICK A CATEGORY</h1>
-        <h3>If. You. Dare.</h3>
+        <h1>BRAIN TUG-OF-WAR</h1>
+        <h2>Instructions:</h2>
+        <h3>Welcome to Brain Tug-Of-War! Select a category to begin answering questions. For each correct answer selected, 
+            you will recieve 1 point added to your total score. For each incorrect answer selected, 1 point will be deducted from your total score. 
+            If you accumulate +7 points, you win! If you fall down to -7 points... YOU LOSE!</h3>
+        <h3>Have fun playing Brain Tug-Of-War!</h3>
+        <h2>Choose Category</h2>
         <div>
             {categories && categories.trivia_categories.map((category, ind) => <li key={ind}>
                 <button onClick={() => setCategoryId(category.id)}>{category.name}</button>
--- react-trivia-michaelgreason/src/gamephase2.js
+++ react-trivia-michaelgreason/src/gamephase2.js
@@ -2,6 +2,8 @@ import {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import he from 'he'
 import Categories from './categories';
+import './GP2.css'
+import { motion } from "framer-motion"
 
 function GamePhase2({categoryId}){
 
@@ -9,47 +11,51 @@ function GamePhase2({categoryId}){
     const [currentQuestion, setCurrentQuestion] = useState(0)
     let score = useRef(0)
     let question = useRef(0)
+    const [remainingQuestions, setRemainingQuestions] = useState(0)
 
     useEffect(() => {
         console.log('useEffect runs')
-        const URL = `https://opentdb.com/api.php?amount=50&category=${categoryId}`
+        const URL = `https://opentdb.com/api.php?amount=41&category=${categoryId}`
         axios.get(URL).then((response) => {           
             setQuestions(response.data)
             score.current = 0
+            setRemainingQuestions(response.data.results.length -1)
         })
             }, [])
                 
                 
                 function handleClick(selectedAnswer) {
-                    console.log(question.correct_answer)
-                    console.log(selectedAnswer)                 
+                    // console.log(question.correct_answer)
+                    // console.log(selectedAnswer)                 
                     if (question.correct_answer === selectedAnswer.target.innerText) {
                     score.current += 1
-                    setCurrentQuestion(currentQuestion => currentQuestion+1)      
+                    setCurrentQuestion(currentQuestion => currentQuestion+1)   
+                    setRemainingQuestions(remainingQuestions => remainingQuestions-1)   
                     } else {
-                        alert('wrong!') 
+                        alert(`Wrong! The correct answer was ${question.correct_answer}`) 
                         score.current -= 1
                         setCurrentQuestion(currentQuestion => currentQuestion+1)
+                        setRemainingQuestions(remainingQuestions => remainingQuestions-1)
                     }
                 }
 
                 function shuffle(answers) {
                     let currentIndex = answers.length,  randomIndex;
-                  
+                
                     // While there remain elements to shuffle.
                     while (currentIndex != 0) {
-                  
-                      // Pick a remaining element.
-                      randomIndex = Math.floor(Math.random() * currentIndex);
-                      currentIndex--;
-                  
-                      // And swap it with the current element.
-                      [answers[currentIndex], answers[randomIndex]] = [
+                
+                    // Pick a remaining element.
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex--;
+                
+                    // And swap it with the current element.
+                    [answers[currentIndex], answers[randomIndex]] = [
                         answers[randomIndex], answers[currentIndex]];
                     }
-                  
+                
                     return answers;
-                  }
+                }
 
                 
 
@@ -67,30 +73,36 @@ function GamePhase2({categoryId}){
                 }
 
                 if (score.current === 7) {
-                    alert('YOU WIN!')
+                    alert('SCORE = 7! YOU WIN!')
                     return <Categories />
                 }
 
                 if (score.current === -7) {
-                    alert('YOU LOSE!')
+                    alert('SCORE = -7! YOU LOSE!')
                     return <Categories />
                 }
                 
+                if (remainingQuestions === 0) {
+                    alert('0 Questions left... STALEMATE!!!')
+                    return <categories />
+                }
                 
                 return (
                     
                 <>
-        
-                <h1>
-                    Question: 
-                </h1>
+            <div className='GP2-heading'>
                 <h2>Score: {score.current}</h2>
-        <div>
-            {questions && <div>{question.question}</div>}
-        </div>
-        <div>
-            {questions && answers.map((selectedAnswer) => <li><button onClick={handleClick}>{selectedAnswer}</button></li>)}
-        </div>
+                <h2>Remaining questions: {remainingQuestions}</h2>
+            </div>
+                <h1>Question: </h1>
+            <div className='q-a'>
+                <div>
+                    {questions && <div>{(he.decode(question.question))}</div>}
+                </div>
+                <div>
+                    {questions && answers.map((selectedAnswer, ind) => <li key={ind}><button onClick={handleClick}>{(he.decode(selectedAnswer))}</button></li>)}
+                </div>
+            </div>
         </>
 
 )}
